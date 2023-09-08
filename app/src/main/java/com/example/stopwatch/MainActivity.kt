9139b5d47f2ce99b92f9c7af465ad7d607394f63
LukasAnell <lukasmanell@gmail.com>
2023-09-08 08:28:29 -0700

    added log messages to overridden lifecycle methods

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,16 +1,25 @@
- package com.example.stopwatch
+package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.Chronometer
+import androidx.constraintlayout.widget.ConstraintLayout
 import kotlin.math.log
 
- class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+    lateinit var layoutMain: ConstraintLayout
+    lateinit var buttonStartStop: Button
+    lateinit var buttonReset: Button
+    lateinit var chronometer: Chronometer
+
     // public static final double PI = 3.14     declaring a classwide constant in java
     // in kotlin, we use a companion object
     companion object {
         // TAG is the default var name for labeling your log statements
         val TAG = "MainActivity"
+
         // just for github testing purposes
         val ASTROPHYSICISTS_PI = 3
     }
@@ -21,30 +30,42 @@ import kotlin.math.log
         Log.d(TAG, "onCreate: this is a log")
     }
 
-     // to override an existing function, just start typing it
-     // can do this for the rest of the lifecycle functions (or any
-     // function that exists in the superclass
-     override fun onStart() {
-         super.onStart()
-     }
+    // to override an existing function, just start typing it
+    // can do this for the rest of the lifecycle functions (or any
+    // function that exists in the superclass
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart: ")
+    }
 
-     override fun onRestart() {
-         super.onRestart()
-     }
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart: ")
+    }
 
-     override fun onResume() {
-         super.onResume()
-     }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume: ")
+    }
 
-     override fun onPause() {
-         super.onPause()
-     }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause: ")
+    }
 
-     override fun onStop() {
-         super.onStop()
-     }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop: ")
+    }
 
-     override fun onDestroy() {
-         super.onDestroy()
-     }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+    }
+
+    fun wireWidgets() {
+        layoutMain = findViewById(R.id.layout_main)
+        buttonStartStop = findViewById(R.id.button_main_startStop)
+        buttonReset = findViewById(R.id.button_main_reset)
+    }
 }
